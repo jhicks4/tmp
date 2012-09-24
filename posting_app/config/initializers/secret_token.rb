f0cc8762dd38f1b829557c9ff4539212531cf3b8
jhicks4 <hicks.jeremy@gmail.com>
2012-09-23 20:03:47 -0400

    rails commit

---
+++ posting_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostingApp::Application.config.secret_token = '357b053826a58dad42ee00a22c795139eb39d9907781fc597fa3705e10e122ff80737daaea54a07705ef6f78d28aedb38703befa73270610523c5a58db573155'
